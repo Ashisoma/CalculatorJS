b00405966a6fea1adae3a2ce99b3ea4086d92771
Onesmus Okali <68568675+Ashisoma@users.noreply.github.com>
2021-07-11 15:44:39 +0300

    slight changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let display = document.getElementById('display');
 
+
 let buttons = Array.from(document.getElementsByClassName('button'));
 
 buttons.map( button => {
@@ -17,11 +18,12 @@ buttons.map( button => {
                 break;
             case '←':
                 if (display.innerText){
-                   display.innerText = display.innerText.slice(0, -1);
+                   display.innerText = display.innerText.slice(0,-1);
                 }
                 break;
             default:
                 display.innerText += e.target.innerText;
         }
     });
-});
+}); 
+
